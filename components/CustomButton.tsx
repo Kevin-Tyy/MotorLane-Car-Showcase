36385c68774ad95e8c4de51af93ee77a3f80cc7e
Kevin TUYIZERE <122741116+Kevin-Tyy@users.noreply.github.com>
2023-07-03 15:55:30 +0200

    Adding public assets

--- components/CustomButton.tsx
+++ components/CustomButton.tsx
@@ -1,9 +1,13 @@
+"use client"
 import React from "react";
 import Image from "next/image";
-const CustomButton = () => {
+import { CustomButtonProps } from "@/types";
+const CustomButton = ({ title , handleClick , containerStyles, btnType} : CustomButtonProps) => {
 	return(
-        <button disabled={false} type={"button"} className={`custom-btn`} onClick={() => {}}>
-            CustomButton
+        <button disabled={false} type={btnType || "button"} className={`custom-btn ${containerStyles}`} onClick={handleClick}>
+            <span className={`flex-1`}>
+                {title}
+            </span>
         </button>
     ) 
 };
