8482a37bf5fd1a714370b4951c71f28cf93576f6
Kevin TUYIZERE <122741116+Kevin-Tyy@users.noreply.github.com>
2023-07-03 22:20:24 +0200

    Card refactor changes

--- components/CarCard.tsx
+++ components/CarCard.tsx
@@ -24,34 +24,38 @@ const CarCard = ({ car }: Props) => {
 				{carRent}
 				<span className="self-end text-[14px] font-medium">/day</span>
 			</p>
-      <div className="relative w-full h-40 my-3 object-contain">
-        <Image src="/hero.png" alt="car" fill priority/> 
-      </div>
-      <div className="relative flex w-full mt-2">
-        <div className="flex group-hover:invisible w-full justify-between text-gray">
-          <div className="flex flex-col justify-center items-center gap-2">
-            <Image src="/tire.svg" width={20} height={20} alt="wheel"/>
-            <p className="text-[14px]">
-              {drive.toUpperCase()}
-            </p>
-          </div>
-          <div className="flex flex-col justify-center items-center gap-2">
-            <Image src="/steering-wheel.svg" width={20} height={20} alt="wheel"/>
-            <p className="text-[14px]">
-              {transmission == "a" ? "Automatic" : "Manual"}
-            </p>
-          </div>
-          <div className="flex flex-col justify-center items-center gap-2">
-            <Image src="/gas.svg" width={20} height={20} alt="wheel"/>
-            <p className="text-[14px]">
-              {city_mpg} MPG
-            </p>
-          </div>
-          <div>
-            
-          </div>
-        </div>
-      </div>
+			<div className="relative w-full h-40 my-3 object-contain">
+				<Image src="/hero.png" alt="car" fill priority />
+			</div>
+			<div className="relative flex w-full mt-2">
+				<div className="flex group-hover:invisible w-full justify-between text-gray">
+					<div className="flex flex-col justify-center items-center gap-2">
+						<Image src="/tire.svg" width={20} height={20} alt="wheel" />
+						<p className="text-[14px]">{drive.toUpperCase()}</p>
+					</div>
+					<div className="flex flex-col justify-center items-center gap-2">
+						<Image
+							src="/steering-wheel.svg"
+							width={20}
+							height={20}
+							alt="wheel"
+						/>
+						<p className="text-[14px]">
+							{transmission == "a" ? "Automatic" : "Manual"}
+						</p>
+					</div>
+					<div className="flex flex-col justify-center items-center gap-2">
+						<Image src="/gas.svg" width={20} height={20} alt="wheel" />
+						<p className="text-[14px]">{city_mpg} MPG</p>
+					</div>
+				</div>
+					<div className="car-card__btn-container">
+						<CustomButton
+							title="View more"
+							containerStyles="w-full py-[16px] rounded-full bg-primary-blue text-white text-[14px] leading-[17px]"
+						/>
+					</div>
+			</div>
 		</div>
 	);
 };
