1a78f594a9c59ee4ad82fea874d8972e83d4db60
Kevin TUYIZERE <122741116+Kevin-Tyy@users.noreply.github.com>
2023-07-04 19:57:22 +0200

    Search component refactor changes

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -1,22 +1,20 @@
-"use client"
+"use client";
 
-import {SearchManufucturer} from "./"
-import { useState } from "react"
+import { SearchManufucturer } from "./";
+import { useState } from "react";
 const SearchBar = () => {
-    const [manufacturer, setManufacturer] = useState('')
-    const handleSearch = () => {
+	const [manufacturer, setManufacturer] = useState("");
+	const handleSearch = () => {};
+	return (
+		<form action="" className="searchbar" onSubmit={handleSearch}>
+			<div className="searchbar__item">
+				<SearchManufucturer
+					manufacturer={manufacturer}
+					setManufacturer={setManufacturer}
+				/>
+			</div>
+		</form>
+	);
+};
 
-    }
-  return (
-    <form action="" className='searchbar' onSubmit={handleSearch}>
-        <div className="searchbar__item">
-            <SearchManufucturer
-                manufacturer={manufacturer}
-                setManufacturer={setManufacturer}
-            />
-        </div>
-    </form>
-  )
-}
-
-export default SearchBar
+export default SearchBar;
