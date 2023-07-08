6bd3b689379dea0ab87282c942dfe205e6b635f9
Kevin TUYIZERE <122741116+Kevin-Tyy@users.noreply.github.com>
2023-07-08 18:07:54 +0200

    Car detai model refactor changes

--- components/CarCard.tsx
+++ components/CarCard.tsx
@@ -26,7 +26,7 @@ const CarCard = ({ car }: Props) => {
 				<span className="self-end text-[14px] font-medium">/day</span>
 			</p>
 			<div className="relative w-full h-40 my-3 object-contain">
-				<Image src="/hero.png" alt="car" fill priority />
+				<Image src="/hero.png" alt="car" fill priority className="object-contain"/>
 			</div>
 			<div className="relative flex w-full mt-2">
 				<div className="flex group-hover:invisible w-full justify-between text-gray">
