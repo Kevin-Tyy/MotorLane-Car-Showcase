f3c742a4cd2475463c83db4048fe2dba708a4c1d
Kevin TUYIZERE <122741116+Kevin-Tyy@users.noreply.github.com>
2023-07-09 15:55:54 +0200

    Search param refactor changes

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -2,6 +2,8 @@
 import Image from "next/image";
 import { SearchManufucturer } from "./";
 import { useState } from "react";
+// import { Router } from "next/router";
+import { useRouter } from "next/navigation";
 const SearchButton = ({ customClasses }: { customClasses: string }) => (
 	<button type="submit" className={`-ml-3 z-10 ${customClasses}`}>
 		<Image
@@ -15,7 +17,31 @@ const SearchButton = ({ customClasses }: { customClasses: string }) => (
 );
 const SearchBar = () => {
 	const [manufacturer, setManufacturer] = useState("");
-	const handleSearch = () => {};
+	const router = useRouter();
+	const handleSearch = (e: React.FormEvent<HTMLFormElement>) => {
+		e.preventDefault();
+		if (manufacturer === "" && model === "") {
+			return alert("Please  fill in the search bar");
+		}
+		updateSearchParams(model.toLowerCase(), manufacturer.toLowerCase());
+	};
+	const updateSearchParams = (model: string, manufacturer: string) => {
+		const searchParams = new URLSearchParams(window.location.search);
+		if (model) {
+			searchParams.set("model", model);
+		} else {
+			searchParams.delete("model");
+		}
+		if (manufacturer) {
+			searchParams.set("manufacturer", model);
+		} else {
+			searchParams.delete("manufacturer");
+		}
+		const newPathname = `${
+			window.location.pathname
+		}?${searchParams.toString()}`;
+		router.push(newPathname);
+	};
 	const [model, setModel] = useState("");
 
 	return (
@@ -34,18 +60,18 @@ const SearchBar = () => {
 						height={25}
 						className="absolute w-[20px] h-[20px] ml-4 "
 					/>
+					<input
+						type="text"
+						name="model"
+						value={model}
+						onChange={(e) => setModel(e.target.value)}
+						placeholder="Tiguan"
+						className="searchbar__input"
+					/>
 				</div>
-				<input
-					type="text"
-					name="model"
-					value={model}t i
-					onChange={(e) => setModel(e.target.value)}
-					placeholder="Tiguan"
-					className="searchbar__input"
-				/>
-				<SearchButton customClasses="sm:hidden"/>
+				<SearchButton customClasses="sm:hidden" />
 			</div>
-			<SearchButton customClasses="max-sm:hidden"/>
+			<SearchButton customClasses="max-sm:hidden" />
 		</form>
 	);
 };
