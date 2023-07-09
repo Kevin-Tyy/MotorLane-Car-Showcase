e897a3ae71a2653306ca3fe37fbdcfc2b5ab8727
Kevin TUYIZERE <122741116+Kevin-Tyy@users.noreply.github.com>
2023-07-09 11:47:40 +0200

    Next config refactor config

--- utils/index.ts
+++ utils/index.ts
@@ -51,7 +51,7 @@ export async function fetchCars() {
 	};
 
 	const response = await fetch(
-		"https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?model=corolla",
+		"https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?model=ferrari ",
 		{
 			headers: headers,
 		}
@@ -83,17 +83,4 @@ export async function fetchCars() {
 //   return result;
 // }
 
-export const generateCarImageUrl = (car: CarProps, angle?: string) => {
-  const url = new URL("https://cdn.imagin.studio/getimage");
-  const { make, model, year } = car;
 
-  url.searchParams.append('customer', process.env.NEXT_PUBLIC_IMAGIN_API_KEY || '');
-  url.searchParams.append('make', make);
-  url.searchParams.append('modelFamily', model.split(" ")[0]);
-  url.searchParams.append('zoomType', 'fullscreen');
-  url.searchParams.append('modelYear', `${year}`);
-  // url.searchParams.append('zoomLevel', zoomLevel);
-  url.searchParams.append('angle', `${angle}`);
-
-  return `${url}`;
-}
